ee94663bb34b798d6f0ae06f96333ee4476519ee
ismailarilik <arilik.ismail@gmail.com>
2021-12-25 13:01:40 +0300

    Add catchAll route for 404 catching

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="not-found">
+    <v-card>
+      <v-card-title>404</v-card-title>
+      <v-card-subtitle>{{ $t('page_not_found') }}</v-card-subtitle>
+    </v-card>
+  </div>
+</template>
